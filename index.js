ee55abe4c74e1f4c004b751bd5154b6e0a7b3383
Isah Ibrahim <isahibn08@gmail.com>
2021-07-20 22:39:03 +0100

    created a none responsive page to be continued tomorrow

---
+++ index.js
@@ -0,0 +1,69 @@
+const simple = document.querySelector('.simple');
+const speedy = document.querySelector('.speedy');
+const easy = document.querySelector('.easy');
+const details = document.querySelector('.slide-details');
+
+function onSimple() {
+  simple.setAttribute('id', 'active');
+  speedy.removeAttribute('id');
+  easy.removeAttribute('id');
+
+  details.innerHTML = `
+      <div class="detail-pic">
+        <div class="rec"></div>
+        <img src="./images/illustration-features-tab-1.svg" alt="">
+      </div>
+      <div class="detail-para">
+        <h1>Bookmark in one click</h1>
+        <p>Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.</p>
+        <div>
+          <button>More Info</button>
+        </div>
+      </div>
+  `;
+
+  details.setAttribute('id', 'details-style');
+}
+function onSpeedy() {
+  speedy.setAttribute('id', 'active');
+  easy.removeAttribute('id');
+  simple.removeAttribute('id');
+
+  details.innerHTML = `
+      <div class="detail-pic">
+        <div class="rec"></div>
+        <img src="./images/illustration-features-tab-2.svg" alt="" style="margin-left:70px;">
+      </div>
+      <div class="detail-para">
+        <h1>Intelligent search</h1>
+        <p>Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks</p>
+        <div>
+          <button>More Info</button>
+        </div>
+      </div>`;
+}
+function onEasy() {
+  easy.setAttribute('id', 'active');
+  speedy.removeAttribute('id');
+  simple.removeAttribute('id');
+
+  details.innerHTML = `
+      <div class="detail-pic">
+        <div class="rec"></div>
+        <img src="./images/illustration-features-tab-3.svg" alt="" style="margin-left:70px;">
+      </div>
+      <div class="detail-para">
+        <h1>Share your bookmarks</h1>
+        <p>Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button</p>
+        <div>
+          <button>More Info</button>
+        </div>
+        
+      </div>`;
+}
+
+simple.addEventListener('click', onSimple);
+speedy.addEventListener('click', onSpeedy);
+easy.addEventListener('click', onEasy);
+
+onSimple();
